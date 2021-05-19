ece8c85152c3c1b8ba12115dbe21fbc170b778fc
Bruno Mendez <bruno.m.275@gmail.com>
2021-05-19 17:06:51 -0500

    input and print

--- vm.py
+++ vm.py
@@ -1,11 +1,14 @@
+from typing import Type
+from errors import TypeMismatchError
 from memory import *
 from constants import *
 
-currentQuad = 0
 
-
-def start(quadruples):
-    global currentQuad
+def start(quadruples, currentQuad=0, inputValue=None):
+    print(currentQuad, inputValue)
+    print("$$$$$")
+    outputCount = 0
+    output = {}
     while currentQuad < len(quadruples):
         print(currentQuad)
         quad = quadruples[currentQuad]
@@ -26,19 +29,42 @@ def start(quadruples):
             result = memory[leftOperand] * memory[rightOperand]
             memory[quad_result] = result
         elif operator == '=':
-            result = memory[leftOperand]
-        elif operator == 'PRINTEXP':
-            print(result)
-        elif operator == 'PRINTSTR':
-            print(quad_result)
+            memory[quad_result] = memory[leftOperand]
+        elif operator == PRINT_EXP:
+            output[outputCount] = memory[quad_result]
+            outputCount += 1
+        elif operator == PRINT_STR:
+            output[outputCount] = quad_result
+            outputCount += 1
+        elif operator == INPUT:
+            if inputValue != None:
+                varType = leftOperand
+                try:
+                    if varType == INT:
+                        inputValue = int(inputValue)
+                    elif varType == FLOAT:
+                        inputValue = float(inputValue)
+                    elif varType == CHAR:
+                        if len(inputValue) != 1:
+                            raise TypeMismatchError
+                except ValueError:
+                    output[outputCount] = "Type mismatch error"
+                    return output
+                memory[quad_result] = inputValue
+                inputValue = None
+            else:
+                output[outputCount] = [INPUT_REQUEST, currentQuad]
+                return output
         elif operator == GOTO:
             if leftOperand == MAIN:
                 currentQuad = quad_result - 1
         elif operator == END_PROG:
-            break
+            output[outputCount] = "FIN!"
+            return output
         currentQuad += 1
         print("$$$")
         print(memory)
         print("@@@")
         print(quad)
         print("###")
+    return output
